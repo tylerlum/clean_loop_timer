dc236128a7b169abd902201157bfb0944343a5b7
Tyler Lum <tylergwlum@gmail.com>
2023-10-07 16:44:28 -0700

    Add url to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 DESCRIPTION = "Easy-to-use timer for profiling complex loops in dataset generation and neural network training"
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
@@ -11,6 +11,7 @@ setup(
     version=VERSION,
     author="Tyler Lum",
     author_email="tylergwlum@gmail.com",
+    url="https://github.com/tylerlum/clean_loop_timer",
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
